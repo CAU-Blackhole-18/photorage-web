76c146e5c55b1b2542b5d7717de12a9f0bfab224
Choi-Ju12g <choi-ju12g@naver.com>
2022-05-14 17:53:41 +0900

    storybook 관련 파일 구조 변경

--- stories/Header.stories.tsx
+++ stories/Header.stories.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 
-import { Header } from './Header';
+import { Header } from '../src/component/Header';
 
 export default {
   title: 'Example/Header',
