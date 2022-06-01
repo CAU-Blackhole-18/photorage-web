3590e284804bdfa9ab6cac083fa6929a0d302ff4
Choi-Ju12g <choi-ju12g@naver.com>
2022-06-01 21:02:13 +0900

    feat: 메인페이지 배경이미지 추가

---
+++ src/components/atoms/BackgroundImage.tsx
@@ -0,0 +1,38 @@
+import styled from "styled-components"
+
+export default function BackgroundImage(){
+
+    return (
+        <Image src="/image/img_mainpageBg.jpg"></Image>
+    )
+}
+
+const Image = styled.img`
+    /* background: url(image/img_mainpageBg) no-repeat 50% 50% fixed; 
+    -webkit-background-size: cover;
+    -moz-background-size: cover;
+    -o-background-size: cover;
+    background-size: cover;   */
+
+    /* Set rules to fill background */
+	min-height: 100%;
+	min-width: 1024px;
+	
+	/* Set up proportionate scaling */
+	width: 100%;
+	height: auto;
+	
+	/* Set up positioning */
+	position: fixed;
+	top: 0;
+	left: 0;
+    
+    z-index: -1;
+
+    @media screen and (max-width: 1024px) { /* Specific to this particular image */
+        img.bg {
+            left: 50%;
+            margin-left: -512px;   /* 50% */
+        }
+    }
+`
