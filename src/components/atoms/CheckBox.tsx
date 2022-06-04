544092f562a2f6488d4f5b955c1908c530b4547f
ki7348 <ki7348@naver.com>
2022-06-04 22:49:48 +0900

    feat 로그인 폼 구현

--- src/components/atoms/CheckBox.tsx
+++ src/components/atoms/CheckBox.tsx
@@ -1,9 +1,60 @@
 import styled from "styled-components";
 
-const CheckBox = () => {
-  return <Container />;
+type CheckBoxPropsType = {
+  checked: boolean;
 };
 
-const Container = styled.input.attrs({ type: "checkbox" })``;
+const CheckBox = (props: CheckBoxPropsType) => {
+  const { checked } = props;
+
+  return (
+    <Container>
+      <HiddenCheckBox type={"checkbox"} checked={checked} />
+      <StyledCheckBox checked={checked}>
+        <Icon viewBox="0 0 24 24">
+          <polyline points="19 7 10 17 5 12" />
+        </Icon>
+      </StyledCheckBox>
+    </Container>
+  );
+};
+
+const Container = styled.div`
+  display: inline-block;
+  vertical-align: middle;
+`;
+
+const Icon = styled.svg`
+  fill: none;
+  stroke: white;
+  stroke-width: 2px;
+`;
+
+const StyledCheckBox = styled.div<{ checked: boolean }>`
+  display: inline-block;
+  cursor: pointer;
+  width: 28px;
+  height: 28px;
+  border: ${(props) => (props.checked ? "none" : "solid 0.1rem #dddddd")};
+  background: ${(props) => (props.checked ? "black" : "white")};
+  border-radius: 100%;
+  transition: all 150ms;
+
+  ${Icon} {
+    visibility: ${(props) => (props.checked ? "visible" : "hidden")};
+  }
+`;
+
+const HiddenCheckBox = styled.input`
+  border: 0;
+  clip: rect(0 0 0 0);
+  height: 1px;
+  margin: -1px;
+  overflow: hidden;
+  padding: 0;
+  position: absolute;
+  white-space: nowrap;
+  width: 1px;
+`;
 
 export default CheckBox;
--- src/components/organisms/auth/LoginForm.tsx
+++ src/components/organisms/auth/LoginForm.tsx
@@ -1,6 +1,12 @@
 import styled from "styled-components";
+import Button from "../../atoms/Button";
+import CheckBox from "../../atoms/CheckBox";
 import InputForm from "../../atoms/InputForm";
 import Spacer from "../../atoms/Spacer";
+import Facebook from "/image/btn_facebook.png";
+import Instagram from "/image/btn_instagram.png";
+import Kakao from "/image/btn_kakao.png";
+import Naver from "/image/btn_naver.png";
 
 const LoginForm = () => {
   return (
@@ -18,6 +24,38 @@ const LoginForm = () => {
         height="56px"
         type="password"
       />
+      <FirstActionWrapper>
+        <LoginMaintainWrapper>
+          <CheckBox checked={false} />
+          <CheckBoxTitle>로그인 유지</CheckBoxTitle>
+        </LoginMaintainWrapper>
+        <Button backgroundColor="transparent">
+          <ButtonGreyContent>비밀번호 찾기</ButtonGreyContent>
+        </Button>
+      </FirstActionWrapper>
+      <Button
+        backgroundColor="#000000"
+        width="400px"
+        height="60px"
+        radius="15px"
+      >
+        <ButtonWhiteContent>이메일로 로그인</ButtonWhiteContent>
+      </Button>
+      <SecondActionWrapper>
+        <Button backgroundColor="transparent">
+          <ButtonBlackContent>회원가입</ButtonBlackContent>
+        </Button>
+        <Button backgroundColor="transparent">
+          <ButtonBlackContent>이메일 찾기</ButtonBlackContent>
+        </Button>
+      </SecondActionWrapper>
+      <SimpleLogin>간편로그인</SimpleLogin>
+      <OAuthWrapper>
+        <Icon src={Naver} />
+        <Icon src={Kakao} />
+        <Icon src={Facebook} />
+        <Icon src={Instagram} />
+      </OAuthWrapper>
     </Container>
   );
 };
@@ -33,7 +71,67 @@ const Container = styled.div`
 
 const Title = styled.h1`
   ${({ theme }) => theme.mixin.fontSize(34, theme.color.main)};
-  margin-bottom: 85px;
+  margin-bottom: 70px;
+`;
+
+const FirstActionWrapper = styled.span`
+  display: flex;
+  justify-content: space-between;
+  width: 400px;
+  margin: 40px auto 55px;
+`;
+
+const LoginMaintainWrapper = styled.span`
+  ${({ theme }) => theme.mixin.flexCenter}
+  display: flex;
+  flex-direction: row;
+`;
+
+const CheckBoxTitle = styled.p`
+  ${({ theme }) => theme.mixin.fontSize(16, theme.color.main)}
+  margin-left: 8px;
+`;
+
+const ButtonGreyContent = styled.span`
+  ${({ theme }) => theme.mixin.fontSize(16, theme.color.grey)};
+  text-decoration: underline;
+  font-weight: 700px;
+`;
+
+const ButtonWhiteContent = styled.span`
+  ${({ theme }) => theme.mixin.fontSize(16, theme.color.light)};
+`;
+
+const SecondActionWrapper = styled.span`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  margin: 48px 0px 65px;
+  width: 200px;
+`;
+
+const ButtonBlackContent = styled.span`
+  ${({ theme }) => theme.mixin.fontSize(16, theme.color.main)};
+  text-decoration: underline;
+`;
+
+const SimpleLogin = styled.p`
+  ${({ theme }) => theme.mixin.fontSize(16, theme.color.grey)};
+`;
+
+const OAuthWrapper = styled.span`
+  width: 400px;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  margin-top: 27px;
+`;
+
+const Icon = styled.img`
+  width: 50px;
+  height: 50px;
 `;
 
 export default LoginForm;
